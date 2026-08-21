9054e8c5c22e36fd97bbc2f224199f0a067bffd2
sankarchellaperumal <csankar90474318@gmail.com>
2026-08-21 09:51:59 +0530

    RDBMS PRN 2  1U25IT091

--- solution.sql
+++ solution.sql
@@ -1,1 +1,10 @@
-
+create database CollegeDB;
+use CollegeDB;
+create table Student (
+    StudentID INT(5) NOT NULL PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT(5) NOT NULL
+);
+describe Student;
